36eb19eaa23022580ba9ae18541a47fd7f4f5780
ths <bruce781@163.com>
2023-01-10 20:48:57 +0800

    Hello 6

---
+++ hello6.js
@@ -0,0 +1,16 @@
+let user = "bruce@tkong.net";
+let password = "bruce";
+let phone = "114514";
+
+if (user=== "bruce@tkong.net" && password==="bruce") {
+    console.log("登入成功");
+}else{
+    console.log("登入失败");
+}
+
+let contact ="phone";
+if(contact === "bruce@tkong.net"||"114514"){
+    console.log("发送验证码");
+}else{
+    console.log("不能发送验证码")
+}
